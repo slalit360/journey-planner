3a7fb2ef3c5be6a367845f32dffd4eae873ff63f
Suthar, Lalit H <lalit.h.suthar@accenture.com>
2019-11-09 11:49:49 +0530

    setting sqlite db conf

--- proj/settings.py
+++ proj/settings.py
@@ -76,13 +76,9 @@ WSGI_APPLICATION = 'proj.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'final',      #db name
-        'USER': 'postgres',  #db user
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
