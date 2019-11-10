c17d0a73fd785a3d5e688ab5dacc726657d5d289
Suthar, Lalit H <lalit.h.suthar@accenture.com>
2019-11-10 11:01:08 +0530

    login form and home page added

--- proj/urls.py
+++ proj/urls.py
@@ -17,9 +17,14 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include # new
 
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import TemplateView # new
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')), # new for login , logout , pass management
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'), # new
 
 ]
 # accounts/ provides below app
